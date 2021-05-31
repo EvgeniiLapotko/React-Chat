3227317907fd954de67995cd2c67178dcdd8c3f8
Evgenii <lapotko9251999984@mail.ru>
2021-05-31 15:51:17 +0300

    start-project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p className="text">text</p>
+    </div>
+  );
+}
+
+export default App;
